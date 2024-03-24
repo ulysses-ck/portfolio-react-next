cffe4c43a137b7e6363bf93a8a258f950be2e5e4
ulysses-ck <ulisesapaza.280@gmail.com>
2024-03-24 01:51:03 -0300

    style(config): set variables according to palette.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,24 @@ const config: Config = {
 				"gradient-conic":
 					"conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
 			},
+			colors: {
+				// dark theme extend
+				"blue-primary": "#03090D",
+				"blue-secondary": "#0D0F10",
+				"blue-tertiary": "#3C40C6",
+
+				// light theme extend
+				"light-blue-primary": "#DAEDFC",
+				"light-blue-secondary": "#E0E0E0",
+				"light-blue-tertiary": "#0BE881",
+			},
+			boxShadow: {
+				// dark theme extend
+				"outer-shadow-primary": "0px 0px 25px 1px #3c40c6",
+
+				// dark theme extend
+				"outer-shadow-primary-two": "0px 0px 25px 3px #0be881",
+			},
 		},
 	},
 	plugins: [],
