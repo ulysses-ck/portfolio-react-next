e6b87d8e05144ce29ffcd665fc0d3e72274c2b1d
ulysses-ck <ulisesapaza.280@gmail.com>
2024-03-25 01:13:29 -0300

    feat(general): add themeswitcher component to index page

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,6 +1,15 @@
 import { useTranslations } from "next-intl";
 
+import dynamic from "next/dynamic";
+
+const ThemeSwitcher = dynamic(() => import("@/components/ThemeSwitcher"));
+
 export default function Index() {
 	const t = useTranslations("IndexPage");
-	return <h1>{t("title")}</h1>;
+	return (
+		<main>
+			<h1>{t("title")}</h1>
+			<ThemeSwitcher />
+		</main>
+	);
 }
