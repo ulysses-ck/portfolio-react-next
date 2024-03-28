aeb86df9b6e15420b4521d3b8111440d635280f9
ulysses-ck <ulisesapaza.280@gmail.com>
2024-03-28 11:39:59 -0300

    feat(landing): add last section

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -18,6 +18,7 @@ import SVGGenericContainer from "@/components/SVGGenericContainer";
 // data
 import { hardSkills, projects } from "@/data";
 import CardWork from "@/components/CardWork";
+import FormContact from "@/components/FormContact";
 
 const LanguageSelect = dynamic(() => import("@/components/LanguageSelect"), {
 	ssr: false,
@@ -33,6 +34,11 @@ const HeaderMobile = dynamic(() => import("@/components/HeaderMobile"), {
 
 export default function Index() {
 	const t = useTranslations("IndexPage");
+
+	async function sendMessage(formData: FormData) {
+		"use server";
+		console.log(formData);
+	}
 
 	return (
 		<>
@@ -53,7 +59,7 @@ export default function Index() {
 					<h1 className="text-4xl text-center">{t("title")}</h1>
 					<p className="text-center">{t("subtitle")}</p>
 				</SectionGeneric>
-				<SectionGeneric className="bg-light-blue-primary dark:bg-blue-primary min-h-lvh flex-shrink-0 p-2 flex flex-col items-center gap-4">
+				<SectionGeneric className="bg-light-blue-primary dark:bg-blue-primary min-h-lvh flex-shrink-0 p-2 flex flex-col items-center justify-center gap-4">
 					<h2>{t("titleAbout")}</h2>
 					<p>{t("descriptionAbout")}</p>
 					<div className={styles.GridHardSkills}>
@@ -77,6 +83,10 @@ export default function Index() {
 					{projects.map((project) => (
 						<CardWork key={project.titleProjectWork} projectWork={project} />
 					))}
+				</SectionGeneric>
+				<SectionGeneric className="flex flex-col items-center gap-4 p-2 min-h-lvh justify-center">
+					<h2>{t("titleContact")}</h2>
+					<FormContact sendMessage={sendMessage}></FormContact>
 				</SectionGeneric>
 			</main>
 		</>
