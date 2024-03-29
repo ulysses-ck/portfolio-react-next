de193a7c58be0bd4e7ab644d6f82f45dfb74a0f7
ulysses-ck <ulisesapaza.280@gmail.com>
2024-03-29 13:57:47 -0300

    feat(components): change the link in corner by entire figure

--- src/components/CardWork/index.tsx
+++ src/components/CardWork/index.tsx
@@ -12,25 +12,24 @@ export default function CardWork({
 }) {
 	return (
 		<article className="flex flex-col rounded-2xl bg-light-blue-primary dark:bg-blue-secondary shadow-md dark:shadow-md">
-			<figure className="relative rounded-t-2xl">
-				<a
-					href={projectWork.urlProject}
-					className="w-[30px] h-[30px] absolute top-0 right-0 flex justify-center items-center bg-yellow-accent-color rounded-tr-2xl"
-				>
-					<SVGGenericContainer width="15px" height="15px">
-						<SVGOpenExternal color="black"></SVGOpenExternal>
-					</SVGGenericContainer>
-				</a>
-				<Image
-					src={projectWork.imageProject.src}
-					width={projectWork.imageProject.width}
-					height={projectWork.imageProject.height}
-					alt={projectWork.imageProject.alt}
-					priority
-					className={`rounded-t-2xl object-cover w-full h-full ${projectWork.imageProject.className}`}
-				/>
-			</figure>
-			<h3 className="text-center">{projectWork.titleProjectWork}</h3>
+			<a href={projectWork.urlProject} className="w-fit h-fit">
+				<figure className="relative rounded-t-2xl">
+					<span className="w-[30px] h-[30px] absolute top-0 right-0 flex justify-center items-center bg-yellow-accent-color rounded-tr-2xl">
+						<SVGGenericContainer width="15px" height="15px">
+							<SVGOpenExternal color="black"></SVGOpenExternal>
+						</SVGGenericContainer>
+					</span>
+					<Image
+						src={projectWork.imageProject.src}
+						width={projectWork.imageProject.width}
+						height={projectWork.imageProject.height}
+						alt={projectWork.imageProject.alt}
+						priority
+						className={`rounded-t-2xl object-cover w-full h-full ${projectWork.imageProject.className}`}
+					/>
+				</figure>
+			</a>
+			<h3 className="text-center text-xl">{projectWork.titleProjectWork}</h3>
 			<ul className="flex gap-2 justify-center p-4">
 				{projectWork.techStack.map((tech) => (
 					<li key={tech.title} title={tech.title}>
